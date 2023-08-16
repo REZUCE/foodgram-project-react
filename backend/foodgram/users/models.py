8346ae3d9d48660f68f66391ec6466ea5eaabaa8
rezuce <rezuce@rezuces-MacBook-Pro.local>
2023-08-16 17:58:05 +0300

    start CustomUser

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """Кастомная модель пользователя."""
+
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        max_length=254,
+        unique=True,
+    )
+
+    username = models.CharField(
+        verbose_name='Уникальный юзернейм',
+        max_length=150,
+        unique=True
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150
+    )
+
+    password = models.CharField(
+        verbose_name='Пароль',
+        max_length=150
+    )
+
+    # Замена логина.
+    USERNAME_FIELD = 'email'
+    # Что будет запрашиваться при createsuperuser
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ("username",)
+
+    def __str__(self):
+        return f"{self.username}: {self.email}"

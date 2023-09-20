2468f66fe699cd2db87b6e1408bcde368fe375bd
rezuce <lisvladimir148@yandex.ru>
2023-09-20 12:50:53 +0300

    fix

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -17,7 +17,7 @@ DEBUG = False
 # Чтобы бэк принимал запросы из nginx добавили host.docker.internal
 # ALLOWED_HOSTS = ['127.0.0.1', 'host.docker.internal', 'localhost', 'backend']
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
